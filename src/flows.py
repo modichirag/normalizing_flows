f3fe944daf17745f16edab281d17e4f78a307cf9
modichirag <cmodi@flatironinstitute.org>
2022-07-21 18:53:49 -0400

    remove memory growth

--- src/flows.py
+++ src/flows.py
@@ -1,8 +1,5 @@
 import numpy as np
 import tensorflow as tf
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-  tf.config.experimental.set_memory_growth(gpu, True)
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 tfb = tfp.bijectors
